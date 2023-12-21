23c47fea387c8c1108d31dd508dea6e235807e39
“fadil-al” <fadil.alfarhan@kaptena.se>
2023-12-21 16:13:12 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import puppeteerRoutes from './routes/puppeteerRoutes';
-import cors from 'cors';
+
 
 
 const app = express();
-app.use(cors()); // This will enable CORS for all routes
+
 const port = process.env.PORT || 3000;
 
 app.use('/puppeteer', puppeteerRoutes);
