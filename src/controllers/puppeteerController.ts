508ea4246503fd08c7d52deead4745434ff82a71
“fadil-al” <fadil.alfarhan@kaptena.se>
2023-12-21 22:20:03 +0100

    test

--- src/controllers/puppeteerController.ts
+++ src/controllers/puppeteerController.ts
@@ -9,37 +9,23 @@ export async function downloadPageAsPdf(req: Request, res: Response) {
         return;
     }
 
-    const chromeOptions = {
-        headless: true,
-        defaultViewport: null,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-first-run",
-            "--no-zygote",
-            "--disable-gpu"
-        ],
-    };
-
     try {
-        const browser = await puppeteer.launch(chromeOptions);
+        const browser = await puppeteer.launch({
+            product: 'firefox',
+            args: ["--no-sandbox"]
+        });
         const page = await browser.newPage();
-        page.setDefaultNavigationTimeout(120000);
-        await page.goto(url, { waitUntil: ['networkidle2', 'domcontentloaded'] });
-        await page.waitForSelector('.ShareRegisterPDF', { visible: true, timeout: 120000 });
+        await page.goto(url, { waitUntil: ['networkidle2'] });
         
         // Generate the PDF
         const pdfBuffer = await page.pdf({ format: 'A4' });
 
         await browser.close();
 
-        // Convert the PDF buffer to a Base64 string
-        const base64String = pdfBuffer.toString('base64');
-
-        // Send the Base64 string to the client
-        res.send(base64String);
+        // Set headers and send the PDF buffer to the client
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=download.pdf');
+        res.send(pdfBuffer);
 
     } catch (error) {
         console.error(error);
