704480ba8c4179cf48f6656f0cf894b6c99855bd
“fadil-al” <fadil.alfarhan@kaptena.se>
2023-12-21 18:18:56 +0100

    test

--- src/controllers/puppeteerController.ts
+++ src/controllers/puppeteerController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
 import puppeteer from 'puppeteer';
-import path from 'path';
-import fs from 'fs';
 
 export async function downloadPageAsPdf(req: Request, res: Response) {
     const url = req.query.url as string;
@@ -10,6 +8,7 @@ export async function downloadPageAsPdf(req: Request, res: Response) {
         res.status(400).send('URL is required');
         return;
     }
+
     const chromeOptions = {
         headless: true,
         defaultViewport: null,
@@ -20,32 +19,23 @@ export async function downloadPageAsPdf(req: Request, res: Response) {
             "--no-zygote"
         ],
     };
+
     try {
         const browser = await puppeteer.launch(chromeOptions);
-        /*
-        const browser = await puppeteer.launch(chromeOptions{
-            headless: 'new',
-            args: [ '--no-sandbox', '--hide-scrollbars', '--disable-web-security' ],
-        });
-        */
         const page = await browser.newPage();
         page.setDefaultNavigationTimeout(60000);
         await page.goto(url, { waitUntil: 'networkidle0' });
 
-        // Define PDF path and options
-        const pdfPath = path.resolve('temp', `download-${Date.now()}.pdf`);
-        await page.pdf({ path: pdfPath, format: 'A4' });
+        // Generate PDF in memory
+        const pdfBuffer = await page.pdf({ format: 'A4' });
 
         await browser.close();
-        // Send the PDF file to the client
-        res.download(pdfPath, (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error sending the file');
-            }
-            // Delete the file after sending to client
-            fs.unlinkSync(pdfPath);
-        });
+
+        // Set headers and send the PDF buffer to the client
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=download.pdf');
+        res.send(pdfBuffer);
+
     } catch (error) {
         console.error(error);
         res.status(500).send('An error occurred');
