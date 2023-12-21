7b77264abe97d423b1b254a8630a149091d1156b
“fadil-al” <fadil.alfarhan@kaptena.se>
2023-12-21 19:57:10 +0100

    test

--- src/controllers/puppeteerController.ts
+++ src/controllers/puppeteerController.ts
@@ -25,7 +25,6 @@ export async function downloadPageAsPdf(req: Request, res: Response) {
         const page = await browser.newPage();
         page.setDefaultNavigationTimeout(120000); // Increase timeout
         await page.goto(url, { waitUntil: ['networkidle2', 'domcontentloaded'] });
-        await page.waitForTimeout(10000); // Additional delay
         
         // Now generate the PDF
         const pdfBuffer = await page.pdf({ format: 'A4' });
