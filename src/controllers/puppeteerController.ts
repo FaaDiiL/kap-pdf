026c57724a8a815717c62971d61e2dc3f28cc19d
“fadil-al” <fadil.alfarhan@kaptena.se>
2023-12-22 17:07:00 +0100

    test

--- src/controllers/puppeteerController.ts
+++ src/controllers/puppeteerController.ts
@@ -1,39 +1,8 @@
 import { Request, Response } from 'express';
 import puppeteer from 'puppeteer';
-import { ParsedQs } from 'qs';
-
-export type Evaluator = () => any[]; // Replace 'any[]' with a more specific type if possible
-
-export async function crawler(url: string, evaluator: Evaluator) {
-  const browser = await puppeteer.launch({
-    headless: true, // Set to true for headless mode
-  });
-  const page = await browser.newPage();
-  await page.goto(url); // Navigate to the specified URL
-
-  const result = await page.evaluate(evaluator);
-
-  await browser.close(); // Close the browser after crawling
-
-  return result;
-}
-
-
-
 
 export async function downloadPageAsPdf(req: Request, res: Response) {
     const url = req.query.url;
-    if (typeof url !== 'string' || !url) {
-        return res.status(400).send('Invalid URL provided');
-    }
-    
-    (async () => {
-        let result = await crawler(url, () => {
-          const nodes = Array.from(document.querySelectorAll('a'));
-          return nodes.map(({ innerText }) => innerText);
-        });
-        console.log(result); // Log the result to the console
-      })();
 
     const configurations = {
         displayHeaderFooter: true,
@@ -46,13 +15,15 @@ export async function downloadPageAsPdf(req: Request, res: Response) {
         printBackground: true,
     }
 
-   
+    if (typeof url !== 'string' || !url) {
+        return res.status(400).send('Invalid URL provided');
+    }
 
     try {
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox', '--hide-scrollbars', '--disable-web-security'],
             ignoreHTTPSErrors: true,
-            headless: true, // Puppeteer defaults to headless mode
+            headless: 'new', // Puppeteer defaults to headless mode
         });
         const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'networkidle0' });
