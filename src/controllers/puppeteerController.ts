abf11ff83c0c1cd067b32c226b972a739259d5f4
“fadil-al” <fadil.alfarhan@kaptena.se>
2023-12-22 09:37:39 +0100

    test

--- src/controllers/puppeteerController.ts
+++ src/controllers/puppeteerController.ts
@@ -4,20 +4,32 @@ import puppeteer from 'puppeteer';
 export async function downloadPageAsPdf(req: Request, res: Response) {
     const url = req.query.url;
 
-    // Ensure url is a string
+    const configurations = {
+        displayHeaderFooter: true,
+        margin: {
+          bottom: 150,
+          left: 50,
+          right: 50,
+          top: 30,
+        },
+        printBackground: true,
+    }
+
     if (typeof url !== 'string' || !url) {
         return res.status(400).send('Invalid URL provided');
     }
 
     try {
         const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--hide-scrollbars', '--disable-web-security'],
+            ignoreHTTPSErrors: true,
+            headless: true // Puppeteer defaults to headless mode
         });
         const page = await browser.newPage();
-        await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
-        
-        const pdf = await page.pdf({ format: 'A4' });
+        await page.goto(url, { waitUntil: 'networkidle0' });
+        const pdf = await page.pdf({ ...configurations,format: 'A4' });
 
+        res.setHeader('Content-Disposition', 'attachment; filename="download.pdf"');
         res.contentType("application/pdf");
         res.send(pdf);
 
